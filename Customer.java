8879a003fcdd82aa159c9215fce793093d07c248
vish k <133127210+vishrk10@users.noreply.github.com>
2026-04-28 14:11:13 +0530

    Customer.java

---
+++ Customer.java
@@ -0,0 +1,18 @@
+public class Customer {
+
+    private int customerId;
+    private String customerName;
+
+    public Customer(int customerId, String customerName) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+}
